bd644da8a992f887e3d0ddc0f7ffab2908db486e
Sukka <isukkaw@gmail.com>
2019-08-10 14:55:10 +0800

    Fix a stupid issue

--- download.sh
+++ download.sh
@@ -20,9 +20,9 @@ echo "Latest Verison: $clash_for_windows_pkg_version"
 
 
 mkdir -p Clash-Windows
-wget -o /dev/null -O Clash-Windows/Clash.for.Windows.Setup.$clash_for_windows_pkg_version.exe "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/$clash_for_windows_pkg_version/Clash.for.Windows.Setup.$clash_for_windows_pkg_version.exe"
+wget -o /dev/null -O Clash-Windows/Clash.for.Windows.Setup.exe "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/$clash_for_windows_pkg_version/Clash.for.Windows.Setup.$clash_for_windows_pkg_version.exe"
 7za a Clash-Windows.7z ./Clash-Windows/*
-[ -d "Clash-Windows.7z" ] && mv -f Clash-Windows.7z ./deploy/Clash-Windows.7z
+[ -f "./Clash-Windows.7z" ] && mv -vf Clash-Windows.7z ./deploy/Clash-Windows.7z
 rm -rf Clash-Windows
 
 
@@ -40,7 +40,7 @@ echo "Latest Verison: $clashx_version"
 
 
 wget -o /dev/null -O ClashX.dmg "https://github.com/yichengchen/clashX/releases/download/$clashx_version/ClashX.dmg"
-[ -d "ClashX.dmg" ] && mv -f ClashX.dmg ./deploy/ClashX.dmg
+[ -f "./ClashX.dmg" ] && mv -vf ClashX.dmg ./deploy/ClashX.dmg
 
 
 sleep 35
@@ -57,7 +57,7 @@ echo "Latest Verison: $ss_android_version"
 
 
 wget -o /dev/null -O ss-android.apk "https://github.com/shadowsocks/shadowsocks-android/releases/download/v$ss_android_version/shadowsocks--universal-$ss_android_version.apk"
-[ -d "ss-android.apk" ] && mv -f ss-android.apk ./deploy/ss-android.apk
+[ -f "./ss-android.apk" ] && mv -vf ss-android.apk ./deploy/ss-android.apk
 
 
 sleep 35
@@ -74,7 +74,7 @@ echo "Latest Verison: $ss_windows_version"
 
 
 wget -o /dev/null -O ss-win.zip "https://github.com/shadowsocks/shadowsocks-windows/releases/download/$ss_windows_version/Shadowsocks-$ss_windows_version.zip"
-[ -d "ss-win.zip" ] && mv -f ss-win.zip ./deploy/ss-win.zip
+[ -f "ss-win.zip" ] && mv -vf ss-win.zip ./deploy/ss-win.zip
 
 
 sleep 35
@@ -91,7 +91,7 @@ echo "Latest Verison: $ss_mac_version"
 
 
 wget -o /dev/null -O ss-mac.zip "https://github.com/shadowsocks/ShadowsocksX-NG/releases/download/v$ss_mac_version/ShadowsocksX-NG.app.$ss_mac_version.zip"
-[ -d "ss-mac.zip" ] && mv -f ss-mac.zip ./deploy/ss-mac.zip
+[ -f "ss-mac.zip" ] && mv -vf ss-mac.zip ./deploy/ss-mac.zip
 
 
 sleep 35
@@ -108,7 +108,7 @@ echo "Latest Verison: $ssd_android_version"
 
 
 wget -o /dev/null -O ssd-android.apk "https://github.com/TheCGDF/SSD-Android/releases/download/$ssd_android_version/shadowsocksd_$ssd_android_version.apk"
-[ -d "ssd-android.apk" ] && mv -f ssd-android.apk ./deploy/ssd-android.apk
+[ -f "ssd-android.apk" ] && mv -vf ssd-android.apk ./deploy/ssd-android.apk
 
 
 sleep 35
@@ -129,7 +129,7 @@ wget -o /dev/null -O ssd-win.zip "https://github.com/TheCGDF/SSD-Windows/release
 unzip ssd-win.zip
 rm -rf ssd-win.zip
 7za a ssd-win.7z ShadowsocksD.exe
-[ -d "ssd-win.7z" ] && mv -f ssd-win.7z ./deploy/ssd-win.7z
+[ -f "ssd-win.7z" ] && mv -vf ssd-win.7z ./deploy/ssd-win.7z
 
 
 sleep 35
@@ -146,7 +146,7 @@ echo "Latest Verison: $ssrr_android_version"
 
 
 wget -o /dev/null -O ssrr-android.apk "https://github.com/shadowsocksrr/shadowsocksr-android/releases/download/$ssrr_android_version/shadowsocksr-android-$ssrr_android_version.apk"
-[ -d "ssrr-android.apk" ] && mv -f ssrr-android.apk ./deploy/ssrr-android.apk
+[ -f "ssrr-android.apk" ] && mv -vf ssrr-android.apk ./deploy/ssrr-android.apk
 
 
 sleep 35
@@ -163,4 +163,4 @@ echo "Latest Verison: $v2rayng_android_version"
 
 
 wget -o /dev/null -O v2rayng.apk "https://github.com/2dust/v2rayNG/releases/download/$v2rayng_android_version/app-universal-release.apk"
-[ -d "v2rayng.apk" ] && mv -f v2rayng.apk ./deploy/v2rayng.apk
+[ -f "v2rayng.apk" ] && mv -vf v2rayng.apk ./deploy/v2rayng.apk
